fa1426110760da4bc988d4b7a51b3f0c0f82df9d
Herberty Freire <134555045+HerbertyFreire@users.noreply.github.com>
2025-07-23 14:04:26 -0300

    Add files via upload
    arquivosotimizados do sistema de calamidades

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+import os
+
+# Caminho para o seu arquivo CSV original (usado apenas na criação do DB)
+ARQUIVO_CSV_ORIGINAL = r'C:\Users\Livia\Desktop\calamidades\dados\tab_cad_01032025_27_20250408.csv'
+
+# Caminho onde o arquivo do banco de dados SQLite será criado/acessado
+# Garante que o DB esteja no mesmo diretório do arquivo CSV original, dentro da pasta 'dados'
+# ou ajusta para onde você realmente quer que ele fique.
+# Por exemplo, para colocar na mesma pasta do script principal:
+# ARQUIVO_DB = 'cadunico.db'
+# Ou manter no local original se for o preferido
+ARQUIVO_DB = os.path.join(os.path.dirname(ARQUIVO_CSV_ORIGINAL), 'cadunico.db')
+
+
+TABELA_PRINCIPAL = 'pessoas' # Nome da tabela no banco de dados
+USUARIOS_FILE = "usuarios.txt" # Arquivo de credenciais de usuários
